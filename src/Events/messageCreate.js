64f4f2edde45056eafffcd6c5070a09c9e63376c
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2023-04-13 21:28:36 -0700

    Fix spam delete crash

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,12 +1,14 @@
 module.exports = {
     async execute(client, message) {
         if(message.channel.id == "1094752253098872943" && !message.author.bot && !message.member.roles.cache.has("1094749656933744751")){
-            message.reply("If you need help, please use <#1094756393745129532>.").then(msg => {
-                setTimeout(() =>{
-                    msg.delete();
-                    message.delete();
-                }, 3000)
-            })
+            try {
+                await message.reply("If you need help, please use <#1094756393745129532>.").then(msg => {
+                    setTimeout(() =>{
+                        msg.delete();
+                        message.delete();
+                    }, 3000)
+                })
+            } catch{}
         }
     },
 };
